37af95dca3c094dd9b79adf58b81df2167d64c31
German Garay <germangarayadriel@hotmail.com>
2017-09-14 11:18:51 -0300

    Añadida imagenes

---
+++ clases/Controlador.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Controlador(object):
+
+    @classmethod
+    def iniciar(cls):
+        pygame.init()
+
+    @classmethod
+    def terminar(cls):
+        pygame.quit()
+        quit()
+
+    @classmethod
+    def configurar_pantalla(cls, ancho, alto):
+        pygame.display.set_caption("Super Mega Mario Bros")
+        return pygame.display.set_mode((ancho,alto)), pygame.FULLSCREEN
+
+    @classmethod
+    def iniciar_reloj(cls):
+        return pygame.time.Clock()
+
+    @classmethod
+    def set_fps(cls, reloj, frames):
+        reloj.tick(frames)
+
+    @classmethod
+    def rellenar_pantalla(cls, ventana, fondo, x, y):
+        ventana.blit(fondo, (x, y))
+
+    @classmethod
+    def buscar_eventos(cls):
+        for evento in pygame.event.get():
+            if evento.type == QUIT:
+                cls.terminar()
+            if evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                cls.terminar(
+
+    @classmethod
+    def buscar_teclas(cls):
+        return pygame.key.get_pressed()
--- main.py
+++ main.py
@@ -1,57 +1,30 @@
 import pygame
+from clases.Controlador import *
 from clases.Mario import Mario
 
 ancho = 1280
 alto = 720
 
-Marios=pygame.sprite.Group()
-
-mario = Mario()
-mario.add(Marios)
-
 main()
 
 def main():
 
-    reloj=pygame.time.Clock()
+    Controlador.iniciar()
+
+    ventana = Controlador.configurar_pantalla(ancho, alto)
+
+    reloj = Controlador.iniciar_reloj()
 
     fondo = pygame.image.load("imagenes/fondo.jpg")
 
-    screen = pygame.display.set_mode((ancho, alto), pygame.FULLSCREEN)
-
-    pygame.display.set_caption("Super Mega Mario Bros")
-
-    x = 0
-    y = 0
-
-    muevePantalla = False
-
     while True:
-        for eventos in pygame.event.get():
-            if eventos.type == pygame.K_ESCAPE:
-                quit()
+
+        Controlador.buscar_eventos()
 
         teclas = pygame.key.get_pressed()
 
-        if muevePantalla == True:
-            x -= 30
-            for item in Activos:
-                item.rect.x -= 30
-            for item in Pisos:
-                item.rect.x -= 30
+        Controlador.rellenar_pantalla(ventana, fondo, 0, 0)
 
-        muevePantalla = mario.mover(teclas, muevePantalla, Activos, Pisos)
-
-        for item in listaSignos:
-            item.devuelve(mario, False, True, Activos, Monedas)
-        for item in listaBloques:
-            item.devuelveBloque(mario)
-
-        if mario.rect.x == 10000:
-            ganarJuego(mario)
-        screen.blit(fondo, (x, y))
-        Activos.draw(screen)
-        Marios.draw(screen)
-        Pisos.draw(screen)
         pygame.display.flip()
-        reloj.tick(60)
+
+        Controlador.set_fps(reloj, 60)
