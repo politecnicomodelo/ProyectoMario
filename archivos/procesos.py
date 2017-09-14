0ce70a597e8556fd234b5eff03d551feab88f878
German Garay <germangarayadriel@hotmail.com>
2017-09-14 16:13:02 -0300

    Funcionan los movimientos, con ciertos errores

--- archivos/procesos.py
+++ archivos/procesos.py
@@ -11,7 +11,7 @@ def procesos(reloj, mario, FPS):
     if teclas[pygame.K_d] or teclas[pygame.K_RIGHT]:
         mario.mover_derecha(30)
 
-    elif teclas[pygame.K_a] or teclas[pygame.K_LEFT]:
+    #elif teclas[pygame.K_a] or teclas[pygame.K_LEFT]:
         mario.mover_izquierda(30)
 
     #if teclas[pygame.K_s] or teclas[pygame.K_DOWN]:
--- clases/Mario.py
+++ clases/Mario.py
@@ -4,21 +4,39 @@ import pygame
 ancho = 1280
 alto = 720
 
-movimientos = {"derecha": "imagenes/mario/mario_der.png", "izquierda": "imagenes/mario/mario_izq.png"}
+movimientos = {"quieto": "imagenes/mario/mario.png",
+               "camina": "imagenes/mario/mario_movimiento.png",
+               "correr": "imagenes/mario/mario_correr.png"}
 
 class Mario(Base):
 
     def __init__(self):
 
-        Base.__init__(self, ancho-1260, 600, 100, 100, "imagenes/mario/mario_der.png")
+        Base.__init__(self, ancho-1260, 600, 100, 100, "imagenes/mario/mario.png")
         self.direccion = True
         Base.sprites.add(self)
+        self.movimiento = 0
 
     def mover_derecha(self, velocidad):
 
         if self.direccion is False:
             self.direccion = True
-            self.image = self.nuevo_sprite(movimientos["derecha"])
+            self.cambiar_sprite(movimientos["quieto"])
+            self.movimiento = 0
+            return
+
+        if self.movimiento == 2:
+            self.cambiar_sprite(movimientos["camina"])
+            self.movimiento = 1
+
+        elif self.movimiento == 1:
+            print("ENTRE")
+            self.cambiar_sprite(movimientos["correr"])
+            self.movimiento = 2
+
+        elif self.movimiento == 0:
+            self.cambiar_sprite(movimientos["camina"])
+            self.movimiento = 1
 
         self.rect.x += velocidad
 
@@ -26,11 +44,37 @@ class Mario(Base):
 
         if self.direccion is True:
             self.direccion = False
-            #self.image = self.nuevo_sprite(movimientos["izquierda"])
+            self.invertir()
+            return
 
+        if self.movimiento == 2:
+            self.cambiar_sprite(movimientos["camina"])
+            self.invertir()
+            self.movimiento = 1
+
+        elif self.movimiento == 1:
+            print("ENTRE")
+            self.cambiar_sprite(movimientos["correr"])
+            self.invertir()
+            self.movimiento = 2
+
+        elif self.movimiento == 0:
+            self.cambiar_sprite(movimientos["camina"])
+            self.invertir()
+            self.movimiento = 1
 
         self.rect.x -= velocidad
 
-    def nuevo_sprite(self, movimiento):
+    def invertir(self):
+
+        self.image = pygame.transform.flip(self.image, True, False)
+        self.image = pygame.transform.scale(self.image, (100, 100))
+
+    def cambiar_sprite(self, movimiento):
+
+        self.image = pygame.image.load(movimiento)
+        self.image = pygame.transform.scale(self.image, (100, 100))
+
+
 
 
